ca00fd9c07d1142e26ad1e20e8622ffc19294064
Felipe Adeildo <contato@felipeadeildo.com>
2024-12-11 20:57:39 -0300

    chore: initialize front and back structure with basic configurations and components

---
+++ front/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-200 text-2xl">
+      Em desenvolvimento...
+    </div>
+  )
+}
+
+export default App
