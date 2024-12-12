32acdee6eca76c9887feeea49aea27725694533f
Felipe Adeildo <contato@felipeadeildo.com>
2024-12-11 21:41:22 -0300

    feat: add SourceIn partial to include additional fields in Prisma schema

--- back/prisma_partials.py
+++ back/prisma_partials.py
@@ -1,3 +1,5 @@
 from prisma.models import Source
 
 Source.create_partial("SourceOut", exclude=["id", "metadata"])
+
+Source.create_partial("SourceIn", include=["id", "metadata", "status", "createdAt"])
