b7351db76eba7a6a606666c0181635004fc9292c
Felipe Adeildo <contato@felipeadeildo.com>
2024-12-11 22:20:26 -0300

    feat: dynamically include routers for modular route management

--- back/main.py
+++ back/main.py
@@ -1,6 +1,16 @@
+import importlib
+
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+routers = ["source"]
+
+for router_name in routers:
+    router_module = importlib.import_module(f"routes.{router_name}")
+    router_prefix = "/".join(router_name.split("."))
+    app.include_router(router_module.router, prefix=f"/{router_prefix}")
 
 
 @app.get("/")
