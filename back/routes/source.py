835a711d0ae53ae9ac4bbefae096731ca196281a
Felipe Adeildo <contato@felipeadeildo.com>
2024-12-14 22:48:42 -0300

    fix: update read_source endpoint to use content_id instead of source_id

--- back/routes/source.py
+++ back/routes/source.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, HTTPException
-
 from dependencies import ESSourceDep
+from fastapi import APIRouter, HTTPException
 from models.source import Source, SourceIn, SourceProvider
 
 router = APIRouter(tags=["Source Manager"])
@@ -11,9 +10,11 @@ async def create_source(source: SourceIn, es_source: ESSourceDep):
     return es_source.create(source)
 
 
-@router.get("/{source_id}", response_model=Source)
-async def read_source(source_id: str, provider: SourceProvider, es_source: ESSourceDep):
-    source = es_source.find_by_provider_and_content_id(provider, source_id)
+@router.get("/", response_model=Source)
+async def read_source(
+    content_id: str, provider: SourceProvider, es_source: ESSourceDep
+):
+    source = es_source.find_by_provider_and_content_id(provider, content_id)
     if not source:
         raise HTTPException(status_code=404, detail="Source not found")
     return source
