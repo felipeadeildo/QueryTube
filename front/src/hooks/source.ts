b862524f36f069500a1f679abd36047b0852e751
Felipe Adeildo <contato@felipeadeildo.com>
2024-12-14 22:53:26 -0300

    feat: integrate @tanstack/react-query for source management hooks

--- front/src/hooks/source.ts
+++ front/src/hooks/source.ts
@@ -1,1 +1,16 @@
-// TODO: tanstack query here.
+import { createSource, readSource } from '@/services/source'
+import { SourceProvider } from '@/types/source'
+import { useMutation, useQuery } from '@tanstack/react-query'
+
+export const useCreateSource = () => {
+  return useMutation({
+    mutationFn: createSource
+  })
+}
+
+export const useReadSource = (contentId: string, provider: SourceProvider) => {
+  return useQuery({
+    queryKey: ['source', contentId, provider],
+    queryFn: () => readSource(contentId, provider)
+  })
+}
