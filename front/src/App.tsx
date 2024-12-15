b8135cac7e30a41da550d6c75ac901b85219ced6
Felipe Adeildo <contato@felipeadeildo.com>
2024-12-14 22:55:13 -0300

    feat: wrap App component with QueryClientProvider for React Query integration

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-200 text-2xl">
-      Em desenvolvimento...
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="flex items-center justify-center h-screen bg-gray-200 text-2xl">
+        Em desenvolvimento...
+      </div>
+    </QueryClientProvider>
   )
 }
 
