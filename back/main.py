4e684f0dc5c9cf8e1d3f952c0e3f6936237f59d3
Felipe Adeildo <contato@felipeadeildo.com>
2024-12-15 23:35:42 -0300

    feat: update API routes to include '/api/' prefix and refactor Tailwind CSS configuration

--- back/main.py
+++ back/main.py
@@ -10,7 +10,7 @@ routers = ["source"]
 for router_name in routers:
     router_module = importlib.import_module(f"routes.{router_name}")
     router_prefix = "/".join(router_name.split("."))
-    app.include_router(router_module.router, prefix=f"/{router_prefix}")
+    app.include_router(router_module.router, prefix=f"/api/{router_prefix}")
 
 
 @app.get("/")
