288248086ef66dfbbb735d873b460abb6cdcd110
Felipe Adeildo <contato@felipeadeildo.com>
2024-12-16 08:40:09 -0300

    fix: read_sources endpoint to accept query parameters for providers and content_ids

--- back/routes/source.py
+++ back/routes/source.py
@@ -1,14 +1,11 @@
-from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from typing import List
+
+from fastapi import APIRouter, HTTPException, Query
 
 from dependencies import ESSourceDep
 from models.source import Source, SourceIn, SourceProvider
 
 router = APIRouter(tags=["Source Manager"])
-
-
-class BatchGetRequest(BaseModel):
-    queries: list[tuple[SourceProvider, str]]
 
 
 @router.post("/", response_model=list[Source])
@@ -20,6 +17,11 @@ async def create_sources(sources: list[SourceIn], es_source: ESSourceDep):
 
 
 @router.get("/", response_model=list[Source])
-async def read_sources(request: BatchGetRequest, es_source: ESSourceDep):
-    results = es_source.find_by_provider_and_content_ids(request.queries)
+async def read_sources(
+    es_source: ESSourceDep,
+    providers: List[SourceProvider] = Query(...),
+    content_ids: List[str] = Query(...),
+):
+    queries = list(zip(providers, content_ids))
+    results = es_source.find_by_provider_and_content_ids(queries)
     return results
